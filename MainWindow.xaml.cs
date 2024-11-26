8607be1d08cbc113e67bf79041f75623e8862c4f
Seonwon Kim <ssun981014@naver.com>
2024-11-26 14:33:02 +0900

    필요없는 이벤트 삭제

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,9 +28,5 @@ namespace ANULink
             this.InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
     }
 }
