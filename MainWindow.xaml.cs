ca34e77f7b08a4f8a173b1c16450e6fb743675de
good-thing <ssun981014@naver.com>
2024-12-03 14:22:43 +0900

    수정중
    ㅂ12

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using System.Collections.ObjectModel;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -21,11 +22,31 @@ namespace ANULink
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
+    public class CustomDataObject
+    {
+        public string ImageLocation { get; set; }
+    }
+
+
     public sealed partial class MainWindow : Window
     {
+        public ObservableCollection<CustomDataObject> ItemsCollection { get; set; }
+
         public MainWindow()
         {
             this.InitializeComponent();
+
+            // 샘플 데이터 추가
+            ItemsCollection = new ObservableCollection<CustomDataObject>
+            {
+            new CustomDataObject { ImageLocation = "Assets/image1.png" },
+            new CustomDataObject { ImageLocation = "Assets/image2.png" },
+            new CustomDataObject { ImageLocation = "Assets/image3.png" }
+            };
+
+            // DataContext 설정
+            this.DataContext = this;
         }
 
     }
